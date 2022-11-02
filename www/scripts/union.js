120e640db29a52a1d6ca71388f5dc503cbde98ce
Iker <llorenteiker99@gmail.com>
2022-11-02 16:53:36 +0100

    Arrays y tuplas TS

---
+++ www/scripts/union.js
@@ -0,0 +1,5 @@
+"use strict";
+// * Tuplas = Array de elementos definidos y de tipos multiples
+let items = ["item"];
+let arrayDeElementos = [1, 3, 4];
+console.log(items);
---
+++ www/ts/union.ts
@@ -0,0 +1,4 @@
+// * Tuplas = Array de elementos definidos y de tipos multiples
+let items: Array<string | number> = ["item"];
+let arrayDeElementos: Array<number> = [1,3,4];
+console.log(items); 
